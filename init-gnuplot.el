61cdd4a397361716d77dd8529adabca9b3bfc778
cbpark <cbpark@gmail.com>
2014-02-17 20:08:35 +0100

    Turn on linum in gnuplot-mode

--- init-gnuplot.el
+++ init-gnuplot.el
@@ -7,6 +7,11 @@
 (autoload 'gnuplot-mode "gnuplot" "gnuplot major mode" t)
 (autoload 'gnuplot-make-buffer "gnuplot" "open a buffer in gnuplot mode" t)
 
+(eval-after-load "gnuplot"
+  '(progn
+     (add-hook 'gnuplot-mode-hook (lambda ()
+                                    (linum-mode 1)))))
+
 (setq auto-mode-alist
   (append
     '(("\\.plt$" . gnuplot-mode)
