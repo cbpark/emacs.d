7746a7789fdb13b915dadd9ac297b3d567ee9687
cbpark <cbpark@gmail.com>
2014-02-27 14:18:00 +0100

    Remove unneccary paths

--- init-load-path.el
+++ init-load-path.el
@@ -1,12 +1,9 @@
 ;;; init-load-path.el --- Paths to load
 ;;; Commentary:
 ;;; Code:
-(add-to-list 'load-path
-             "/Applications/MacPorts/Emacs.app/Contents/Resources/lisp")
-(add-to-list 'load-path "/opt/local/share/emacs/site-lisp")
+
 (add-to-list 'load-path "~/.emacs.d/site-lisp")
 (add-to-list 'load-path "~/.emacs.d/site-lisp/custom-theme")
 
 (provide 'init-load-path)
-
 ;;; init-load-path.el ends here
