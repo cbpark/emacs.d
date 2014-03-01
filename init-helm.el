b2c945f10b68b87781837c028f699b1ebed08caf
cbpark <cbpark@gmail.com>
2014-03-01 17:39:52 +0100

    Remove helm-c-yasnippet and ac-helm

--- init-helm.el
+++ init-helm.el
@@ -6,52 +6,13 @@
 
 (require 'helm-config)
 (require 'helm-utils)
+(helm-mode 1)
 
 ;; Yanking text
 (setq helm-yank-symbol-first 't)
 
 ;; Buffer File Completion
 (setq enable-recursive-minibuffers t)
-
-;; helm projectile
-(require-package 'helm-projectile)
-(eval-after-load "projectile"
-  '(progn
-     (define-key projectile-mode-map (kbd "C-c h") 'helm-projectile)))
-
-;; helm-swoop
-(require-package 'helm-swoop)
-(global-set-key (kbd "M-i")     'helm-swoop)
-(global-set-key (kbd "M-I")     'helm-swoop-back-to-last-point)
-(global-set-key (kbd "C-c M-i") 'helm-multi-swoop)
-(global-set-key (kbd "C-x M-i") 'helm-multi-swoop-all)
-;; Save buffer when helm-multi-swoop-edit complete
-(setq helm-multi-swoop-edit-save t)
-;; If this value is t, split window inside the current window
-(setq helm-swoop-split-with-multiple-windows nil)
-;; Split direcion. 'split-window-vertically or 'split-window-horizontally
-(setq helm-swoop-split-direction 'split-window-vertically)
-(eval-after-load "helm-swoop"
-  '(progn
-     ;; When doing isearch, hand the word over to helm-swoop
-     (define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
-     ;; From helm-swoop to helm-multi-swoop-all
-     (define-key helm-swoop-map (kbd "M-i") 'helm-multi-swoop-all-from-helm-swoop)))
-
-;; ac-helm
-(require-package 'ac-helm)
-(eval-after-load 'auto-complete
-  '(define-key ac-complete-mode-map (kbd "C-:") 'ac-complete-with-helm))
-
-;; helm-flycheck
-(require-package 'helm-flycheck)
-(eval-after-load 'flycheck
-  '(define-key flycheck-mode-map (kbd "C-c ! h") 'helm-flycheck))
-
-;; helm-c-yasnippet
-(require-package 'helm-c-yasnippet)
-(setq helm-yas-space-match-any-greedy t)
-(global-set-key (kbd "C-c y") 'helm-yas-complete)
 
 ;; key bindings
 (global-set-key (kbd "M-x")     'helm-M-x)
@@ -62,7 +23,35 @@
 (global-set-key (kbd "C-x b")   'helm-buffers-list)
 (global-set-key (kbd "M-y")     'helm-show-kill-ring)
 
-(helm-mode 1)
+;; helm-swoop
+(require-package 'helm-swoop)
+(global-set-key (kbd "M-i")     'helm-swoop)
+(global-set-key (kbd "M-I")     'helm-swoop-back-to-last-point)
+(global-set-key (kbd "C-c M-i") 'helm-multi-swoop)
+(global-set-key (kbd "C-x M-i") 'helm-multi-swoop-all)
+(eval-after-load "helm-swoop"
+  '(progn
+     ;; Save buffer when helm-multi-swoop-edit complete
+     (setq helm-multi-swoop-edit-save t)
+     ;; If this value is t, split window inside the current window
+     (setq helm-swoop-split-with-multiple-windows nil)
+     ;; Split direcion. 'split-window-vertically or 'split-window-horizontally
+     (setq helm-swoop-split-direction 'split-window-vertically)
+     ;; When doing isearch, hand the word over to helm-swoop
+     (define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
+     ;; From helm-swoop to helm-multi-swoop-all
+     (define-key helm-swoop-map (kbd "M-i") 'helm-multi-swoop-all-from-helm-swoop)))
+
+;; helm-flycheck
+(require-package 'helm-flycheck)
+(eval-after-load 'flycheck
+  '(define-key flycheck-mode-map (kbd "C-c ! h") 'helm-flycheck))
+
+;; helm projectile
+(require-package 'helm-projectile)
+(eval-after-load "projectile"
+  '(progn
+     (define-key projectile-mode-map (kbd "C-c h") 'helm-projectile)))
 
 (provide 'init-helm)
 ;;; init-helm.el ends here
