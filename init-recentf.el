e854a53d66c19b6d6f184e2e568b807038132cca
cbpark <cbpark@gmail.com>
2014-03-01 01:56:35 +0100

    Update recentf-exclude

--- init-recentf.el
+++ init-recentf.el
@@ -5,18 +5,19 @@
 (require 'recentf)
 (setq recentf-auto-cleanup 'never)
 (recentf-mode 1)
-(setq recentf-max-menu-items 10)
+(setq recentf-max-menu-items 20)
 
 (setq recentf-exclude
-      '("\\.ido.last$" "^autoloads.el$" "^archive-contents$" "^.*/bookmarks$"
-        "^.*/cookies$" "\\.html$" "\\.cache$" "^.*/elpa/.*$" "^.*/Maildir/.*$"))
+      '("\\.ido.last$" "^autoloads.el$" "^archive-contents$" "^.*\/bookmarks$"
+        "^.*\/cookies$" "\\.html$" "\\.cache$" "^.*\/elpa\/.*$"
+        "^.*\/Maildir\/.*$" "^.*\/\.git\/.*$"))
 
-(defun recentf-ido-find-file ()
-  "Find a recent file using Ido."
-  (interactive)
-  (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
-    (when file
-      (find-file file))))
+;; (defun recentf-ido-find-file ()
+;;   "Find a recent file using Ido."
+;;   (interactive)
+;;   (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
+;;     (when file
+;;       (find-file file))))
 
 ;; (global-set-key (kbd "C-x C-r") 'recentf-ido-find-file)
 
