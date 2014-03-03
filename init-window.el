ecf7ff85c75e08fbf652574da0b00b2097f251c4
cbpark <cbpark@gmail.com>
2014-03-03 15:58:41 +0100

    Delete init files

--- init-window.el
+++
@@ -1,9 +0,0 @@
-;;; init-window.el --- Move between windows easily
-;;; Commentary:
-;;; Code:
-
-(require-package 'switch-window)
-(global-set-key (kbd "C-x o") 'switch-window)
-
-(provide 'init-window)
-;;; init-window.el ends here
