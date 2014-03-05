5a81e24505966e72d69aa95ea671e550d929829d
cbpark <cbpark@gmail.com>
2014-03-05 03:08:42 +0100

    Modify init-auto-complete

--- lisp/init-auto-complete.el
+++ lisp/init-auto-complete.el
@@ -4,21 +4,21 @@
 
 (require-package 'auto-complete)
 (require 'auto-complete-config)
-
+(ac-config-default)
 (global-auto-complete-mode t)
-(setq ac-auto-start 4)
+(setq ac-auto-start 3)
 (setq ac-ignore-case t)
 (setq ac-use-menu-map t)
-(setq ac-quick-help-delay 0.5)
-(ac-set-trigger-key "TAB")
-(ac-config-default)
+(setq ac-delay 0.2)
+(setq ac-quick-help-delay 0.2)
+(setq ac-use-fuzzy t)
+(setq tab-always-indent 'complete)
 
-;; Finish completion by TAB
-(define-key ac-completing-map "\t" 'ac-complete)
-(define-key ac-completing-map "\r" nil)
-
+(define-key ac-completing-map    (kbd "RET") 'ac-complete)
 (define-key ac-complete-mode-map (kbd "C-n") 'ac-next)
 (define-key ac-complete-mode-map (kbd "C-p") 'ac-previous)
+(define-key ac-completing-map    "\t"        'ac-expand-common)
+
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 
 ;; ac-ispell
