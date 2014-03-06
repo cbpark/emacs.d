1bb2c56f32c58cba0dacdb1d93b997f55df594b8
cbpark <cbpark@gmail.com>
2014-03-06 02:14:51 +0100

    Update init-haskell

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -3,15 +3,24 @@
 ;;; Code:
 
 (require-package 'haskell-mode)
-(add-to-list 'auto-mode-alist '("\\.hs\\'" . haskell-mode))
-(autoload 'haskell-mode "haskell-mode" "Haskell mode" t)
 
-(require-package 'ghc)
-(autoload 'ghc-init "ghc" nil t)
-(add-hook-fn haskell-mode-hook (ghc-init))
+(autoload 'haskell-mode "haskell-mode" "Haskell Mode" t)
+(setq auto-mode-alist
+      (append
+       '(("\\.hs$"    . haskell-mode)
+         ("\\.hsc$"   . haskell-mode)
+         ("\\.cpphs$" . haskell-mode)) auto-mode-alist))
+
+(autoload 'haskell-cabal-mode "haskell-cabal-mode" "Haskell Cabal Mode" t)
+(add-to-list 'auto-mode-alist '("\\.cabal\\'" . haskell-cabal-mode))
 
 ;; Ignore compiled Haskell files in filename completions
 (add-to-list 'completion-ignored-extensions ".hi")
+
+(require-package 'ghc)
+(autoload 'ghc-init "ghc" nil t)
+(dolist (mode '("haskell-mode" "haskell-cabal-mode"))
+  (eval-after-load mode '(ghi-init)))
 
 ;; Haskell doc mode
 (dolist (hook '(haskell-mode-hook inferior-haskell-mode-hook))
@@ -22,45 +31,29 @@
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 
-;; linum-mode
-(add-hook-fn haskell-mode-hook (linum-mode 1))
-
-;; Yasnippet
-(add-hook-fn haskell-mode-hook (yas-minor-mode))
-
-;; Interactive Block Indentation
+;; Key bindings
 (eval-after-load "haskell-mode"
   '(progn
-     (define-key haskell-mode-map (kbd "C-,") 'haskell-move-nested-left)
-     (define-key haskell-mode-map (kbd "C-.") 'haskell-move-nested-right)))
+     (define-key haskell-mode-map (kbd "C-,")     'haskell-move-nested-left)
+     (define-key haskell-mode-map (kbd "C-.")     'haskell-move-nested-right)
+     (define-key haskell-mode-map (kbd "C-c C-c") 'haskell-compile)
+     (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
+     (define-key haskell-mode-map (kbd "C-c C-b") 'haskell-interactive-switch)
+     (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+     (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+     (define-key haskell-mode-map (kbd "C-c M-.") nil)
+     (define-key haskell-mode-map (kbd "C-c C-d") nil)))
 
-;; Compilation
-(eval-after-load "haskell-mode"
-    '(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-compile))
-
-(eval-after-load "haskell-cabal"
-    '(define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-compile))
+(eval-after-load "haskell-cabal-mode"
+  '(progn
+     (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-compile)
+     (define-key haskell-cabal-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
+     (define-key haskell-cabal-mode-map (kbd "C-c C-b") 'haskell-interactive-switch)))
 
 ;; GHCi and Haskell interactive mode
 (require-package 'ghci-completion)
-(add-hook 'inferior-haskell-mode-hook 'turn-on-ghci-completion)
-(add-hook 'haskell-interactive-mode-hook 'turn-on-ghci-completion)
-(add-hook 'haskell-interactive-mode-hook (lambda ()
-                                           (auto-complete-mode 1)))
-(add-hook-fn haskell-interactive-mode-hook (setq global-hl-line-mode nil))
-(setq haskell-program-name "ghci")
-
-;; haskell-interactive-mode
-(eval-after-load "haskell-mode"
-  '(progn
-    (define-key haskell-mode-map (kbd "C-x C-d") nil)
-    (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
-    (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
-    (define-key haskell-mode-map (kbd "C-c C-b") 'haskell-interactive-switch)
-    (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
-    (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
-    (define-key haskell-mode-map (kbd "C-c M-.") nil)
-    (define-key haskell-mode-map (kbd "C-c C-d") nil)))
+(dolist (hook '(inferior-haskell-mode-hook haskell-interactive-mode-hook))
+  (add-hook hook #'(lambda () (turn-on-ghci-completion))))
 
 ;; flycheck-haskell
 (add-hook 'haskell-mode-hook 'flycheck-mode)
@@ -71,9 +64,23 @@
 ;; flyspell
 ;; (add-hook 'haskell-mode-hook 'flyspell-prog-mode)
 
-;; Customizations
-(setq haskell-stylish-on-save t
-      haskell-tags-on-save t)
+;; Customizations and hooks
+(eval-after-load "haskell-mode"
+  '(setq haskell-program-name "ghci"
+         haskell-stylish-on-save t
+         haskell-tags-on-save t))
+
+(eval-after-load "haskell-cabal-mode"
+  '(setq haskell-program-name "ghci"))
+
+(add-hook 'haskell-mode-hook #'(lambda ()
+                                 (linum-mode 1)
+                                 (yas-minor-mode)))
+
+(dolist (hook '(inferior-haskell-mode-hook haskell-interactive-mode-hook))
+  (add-hook hook #'(lambda ()
+                     (auto-complete-mode 1)
+                     (setq global-hl-line-mode nil))))
 
 (provide 'init-haskell)
 ;;; init-haskell.el ends here
