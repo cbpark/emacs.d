70b2928b5da9faf734a9188bcc95f06b13724cc6
cbpark <cbpark@gmail.com>
2014-03-10 00:45:33 +0100

    Turn on linum in markdown mode

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -14,5 +14,7 @@
      (setq markdown-command "multimarkdown")
      (add-hook 'markdown-mode-hook 'turn-off-auto-fill)))
 
+(add-hook 'markdown-mode-hook #'(lambda () (linum-mode 1)))
+
 (provide 'init-markdown)
 ;;; init-markdown.el ends here
