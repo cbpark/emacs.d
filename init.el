f8116569499f6d211e14623d86478ca8aa6c851e
cbpark <cbpark@gmail.com>
2014-03-15 03:23:01 +0100

    Remove .emacs.d in load-path

--- init.el
+++ init.el
@@ -15,10 +15,8 @@
 
 (unless (boundp 'user-emacs-directory)
   (defvar user-emacs-directory "~/.emacs.d/"))
-(add-to-list 'load-path user-emacs-directory)
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-
 (add-to-list 'load-path (expand-file-name "site-lisp" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "site-lisp/custom-theme" user-emacs-directory))
 
