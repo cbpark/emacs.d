4ab5885d13b0fab5a2b482a733b03123b95e6802
cbpark <cbpark@gmail.com>
2014-05-08 17:12:38 +0200

    Turn on winner-mode

--- lisp/init-window.el
+++ lisp/init-window.el
@@ -5,5 +5,10 @@
 (require-package 'switch-window)
 (global-set-key (kbd "C-x o") 'switch-window)
 
+(when (fboundp 'winner-mode)
+      (winner-mode 1)
+      (global-set-key (kbd "C-x 0") 'winner-undo)
+      (global-set-key (kbd "C-x 9") 'winner-redo))
+
 (provide 'init-window)
 ;;; init-window.el ends here
