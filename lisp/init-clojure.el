7f7983d06589691c55716048d151afedaf5f8d09
cbpark <cbpark@gmail.com>
2014-07-26 22:11:58 +0200

    Remove cider

--- lisp/init-clojure.el
+++ lisp/init-clojure.el
@@ -4,33 +4,13 @@
 
 (require-package 'clojure-mode)
 
+(autoload 'clojure-mode "clojure" "Major mode for Clojure." t)
+(setq auto-mode-alist (append '(("\\.clj\\'" . lisp-mode)) auto-mode-alist))
+
 (add-hook 'clojure-mode-hook
           (lambda ()
-            ;; (linum-mode 1)
+            (linum-mode 1)
             (enable-paredit-mode)))
-(setq clojure-defun-style-default-indent t)
-
-;; cider and ac-nrepl
-(require-package 'cider)
-
-(add-hook 'cider-repl-mode-hook
-          (lambda ()
-            (linum-mode -1)
-            (enable-paredit-mode)))
-(setq nrepl-hide-special-buffers t)
-
-;; ac-nrepl
-(require-package 'ac-nrepl)
-
-(add-hook 'cider-repl-mode-hook 'ac-nrepl-setup)
- (add-hook 'cider-interaction-mode-hook 'ac-nrepl-setup)
- (eval-after-load "auto-complete"
-   '(add-to-list 'ac-modes 'cider-repl-mode))
-
-(add-hook 'cider-repl-mode-hook
-          'set-auto-complete-as-completion-at-point-function)
-(add-hook 'cider-interaction-mode-hook
-          'set-auto-complete-as-completion-at-point-function)
 
 (provide 'init-clojure)
 ;;; init-clojure.el ends here
