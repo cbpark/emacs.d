461303b864d3b5341061f9c9dbd77b6f918d5fc8
cbpark <cbpark@gmail.com>
2014-08-02 03:12:50 +0200

    Use haskell-indentation instead of haskell-indent

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -23,8 +23,8 @@
 
 ;; Indentation
 ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 
 (defun haskell-insert-import ()
   "Insert import."
