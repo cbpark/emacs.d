2047e8eed2536ecec2888ab4a7c4558f32f116c8
cbpark <cbpark@gmail.com>
2014-10-12 19:14:28 +0900

    Remove auto-complete and add company-auctex in LaTeX mode

--- lisp/init-auctex.el
+++ lisp/init-auctex.el
@@ -35,7 +35,6 @@
                                     (flyspell-mode)
                                     (linum-mode 1)
                                     (turn-on-reftex)
-                                    (ac-ispell-ac-setup)
                                     (rainbow-delimiters-mode-enable)))))
 
 (eval-after-load 'reftex
@@ -46,15 +45,9 @@
 ;; doc-view with auto-revert to review output
 (add-hook 'doc-view-mode-hook 'auto-revert-mode)
 
-(eval-after-load 'latex
-  '(progn
-     (defun ac-latex-mode-setup ()
-       (setq ac-sources
-             (append '(ac-source-math-unicode
-                       ac-source-math-latex
-                       ac-source-latex-commands) ac-sources)))
-     (add-hook 'LaTex-mode-hook 'ac-latex-mode-setup)
-     (ac-flyspell-workaround)))
+;; company-auctex
+(require-package 'company-auctex)
+(company-auctex-init)
 
 (provide 'init-auctex)
 ;;; init-auctex.el ends here
