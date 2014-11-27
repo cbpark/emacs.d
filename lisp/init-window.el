7976a3cb241b1e1153a0a60e3d3a50c7cbc799d5
cbpark <cbpark@gmail.com>
2014-11-28 01:22:46 +0900

    Remove switch-window

--- lisp/init-window.el
+++ lisp/init-window.el
@@ -2,9 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-(require-package 'switch-window)
-(global-set-key (kbd "C-x o") 'switch-window)
-
 (when (fboundp 'winner-mode)
       (winner-mode 1)
       (global-set-key (kbd "C-x 0") 'winner-undo)
