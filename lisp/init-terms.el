771ebd27843ba8918ee83980ec6ac4e79e08d7e3
cbpark <cbpark@gmail.com>
2014-12-29 23:25:13 +0900

    Remove my-eshell-open function

--- lisp/init-terms.el
+++ lisp/init-terms.el
@@ -42,22 +42,6 @@
 ;; Add color to a shell
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 
-(defun my-eshell-open ()
-  "Opens up a shell in the directory associated with the current buffer."
-  (interactive)
-  (let* ((parent (if (buffer-file-name)
-                     (file-name-directory (buffer-file-name))
-                   default-directory))
-         (height (/ (window-total-height) 2))
-         (name   (car (last (split-string parent "/" t)))))
-    (split-window-vertically (- height))
-    (other-window 1)
-    (eshell "new")
-    (rename-buffer (concat "*eshell: " name "*"))
-
-    (insert (concat "ls"))
-    (eshell-send-input)))
-
 (global-set-key (kbd "C-c e") 'my-eshell-open)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
