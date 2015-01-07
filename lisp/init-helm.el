6775e204238d6bfa07b5985a3ff35bab028d1d0f
cbpark <cbpark@gmail.com>
2015-01-07 23:10:27 +0900

    Set helm-autoresize-max/min-height

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -13,7 +13,10 @@
 (global-unset-key (kbd "C-x c"))
 
 (helm-mode 1)
+
 (helm-autoresize-mode t)
+(setq helm-autoresize-max-height 30
+      helm-autoresize-min-height 30)
 
 (eval-after-load 'helm
   '(progn
@@ -52,19 +55,19 @@
      (define-key helm-grep-mode-map (kbd "p") 'helm-grep-mode-jump-other-window-backward)))
 
 ;; key bindings
-(global-set-key (kbd "M-x")       'helm-M-x)
-(global-set-key (kbd "M-y")       'helm-show-kill-ring)
-(global-set-key (kbd "C-x b")     'helm-mini)
-(global-set-key (kbd "C-x C-f")   'helm-find-files)
-(global-set-key (kbd "C-x C-r")   'helm-recentf)
-(global-set-key (kbd "C-c h s")   'helm-semantic-or-imenu)
-(global-set-key (kbd "C-c h m")   'helm-man-woman)
-(global-set-key (kbd "C-c h f")   'helm-find)
-(global-set-key (kbd "C-c h l")   'helm-locate)
-(global-set-key (kbd "C-c h g")   'helm-do-grep)
-(global-set-key (kbd "C-c h o")   'helm-occur)
-(global-set-key (kbd "C-c h r")   'helm-resume)
-(global-set-key (kbd "C-c SPC")   'helm-all-mark-rings)
+(global-set-key (kbd "M-x")     'helm-M-x)
+(global-set-key (kbd "M-y")     'helm-show-kill-ring)
+(global-set-key (kbd "C-x b")   'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x C-r") 'helm-recentf)
+(global-set-key (kbd "C-c h s") 'helm-semantic-or-imenu)
+(global-set-key (kbd "C-c h m") 'helm-man-woman)
+(global-set-key (kbd "C-c h f") 'helm-find)
+(global-set-key (kbd "C-c h l") 'helm-locate)
+(global-set-key (kbd "C-c h g") 'helm-do-grep)
+(global-set-key (kbd "C-c h o") 'helm-occur)
+(global-set-key (kbd "C-c h r") 'helm-resume)
+(global-set-key (kbd "C-c SPC") 'helm-all-mark-rings)
 
 ;; helm-swoop
 (require-package 'helm-swoop)
@@ -80,6 +83,9 @@
      (setq helm-swoop-split-with-multiple-windows nil)
      ;; Split direcion. 'split-window-vertically or 'split-window-horizontally
      (setq helm-swoop-split-direction 'split-window-vertically)
+     ;; Optional face for line numbers
+     ;; Face name is `helm-swoop-line-number-face`
+     (setq helm-swoop-use-line-number-face t)
      ;; When doing isearch, hand the word over to helm-swoop
      (define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
      ;; From helm-swoop to helm-multi-swoop-all
