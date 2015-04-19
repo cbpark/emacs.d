b8607d97321dbafebe6b18a5ef388c37b3853fb3
cbpark <cbpark@gmail.com>
2015-04-19 19:03:54 +0900

    Back to zenburn theme

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -3,14 +3,19 @@
 ;;; Code:
 
 ;; zenburn theme
-;; (require-package 'zenburn-theme)
-;; (load-theme 'zenburn t)
+(require-package 'zenburn-theme)
+(load-theme 'zenburn t)
 ;; monokai theme
 ;; (require-package 'monokai-theme)
 ;; (load-theme 'monokai t)
 ;; base16 theme
-(require-package 'base16-theme)
-(load-theme 'base16-default t)
+;; (require-package 'base16-theme)
+;; (load-theme 'base16-default t)
+
+;; Transparent background if inactive
+;; (unless (eq window-system nil)
+;;   (set-frame-parameter (selected-frame) 'alpha '(100 85))
+;;   (add-to-list 'default-frame-alist '(alpha 100 85)))
 
 (provide 'init-themes)
 ;;; init-themes.el ends here
