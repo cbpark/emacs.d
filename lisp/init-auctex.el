8cb60f77e5c7baa38b20fa4b7068143bdc4a5956
cbpark <cbpark@gmail.com>
2015-09-25 23:43:49 +0900

    Add latex-preview-pane

--- lisp/init-auctex.el
+++ lisp/init-auctex.el
@@ -48,5 +48,8 @@
 ;; doc-view with auto-revert to review output
 (add-hook 'doc-view-mode-hook 'auto-revert-mode)
 
+;; latex-preview-pane
+(require-package 'latex-preview-pane)
+
 (provide 'init-auctex)
 ;;; init-auctex.el ends here
