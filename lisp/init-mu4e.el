1286ecf4d366939434b632a381c051d01bff6f96
cbpark <cbpark@gmail.com>
2016-01-28 18:55:00 +0900

    Add label to mu4e

--- lisp/init-mu4e.el
+++ lisp/init-mu4e.el
@@ -26,8 +26,9 @@
                ("/cbpark_gmail.com/[Google Mail].Sent Mail"  . ?s)
                ("/cbpark_gmail.com/[Google Mail].Trash"      . ?t)
                ("/cbpark_gmail.com/arxiv.org"                . ?a)
+               ("/cbpark_gmail.com/github.com"               . ?g)
                ("/cbpark_gmail.com/Haskell-cafe"             . ?h)
-               ("/cbpark_gmail.com/github.com"               . ?g)))
+               ("/cbpark_gmail.com/reddit.com"               . ?r)))
 
        ;; allow for updating mail using 'U' in the main view:
        (setq mu4e-get-mail-command "offlineimap -q")
