62b24415c649fcb77fba2217e9969169d228a479
cbpark <cbpark@gmail.com>
2016-02-01 02:18:36 +0900

    Enable transparent background in darwin

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -5,15 +5,12 @@
 ;; zenburn theme
 ;; (require-package 'zenburn-theme)
 ;; (load-theme 'zenburn t)
-;; monokai theme
-;; (require-package 'monokai-theme)
-;; (load-theme 'monokai t)
 ;; base16 theme
 (require-package 'base16-theme)
 (load-theme 'base16-default-dark t)
 
-;; Transparent background if inactive
-(when (and (string-equal system-type "gnu/linux") window-system)
+;; Transparent background.
+(when window-system
   (set-frame-parameter (selected-frame) 'alpha '(95 95))
   (add-to-list 'default-frame-alist '(alpha 95 95)))
 
@@ -25,7 +22,7 @@
 (add-hook 'window-setup-hook 'on-frame-open)
 
 ;; Font
-(unless (eq window-system nil)
+(when window-system
   (if (string-equal system-type "darwin")
       (progn
         (setq initial-frame-alist '((font . "Source Code Pro 12")))
