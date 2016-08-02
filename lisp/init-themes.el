d3a21c7eb1a039114035977dbed70e237b5ade47
cbpark <cbpark@gmail.com>
2016-08-03 00:41:41 +0900

    Use source code pro font in Linux

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -28,8 +28,8 @@
         (setq initial-frame-alist '((font . "Source Code Pro 12")))
         (setq default-frame-alist '((font . "Source Code Pro 12"))))
     (progn
-      (setq initial-frame-alist '((font . "Ubuntu Mono 10")))
-      (setq default-frame-alist '((font . "Ubuntu Mono 10")))))
+      (setq initial-frame-alist '((font . "Source Code Pro 9")))
+      (setq default-frame-alist '((font . "Source Code Pro 9")))))
   (set-fontset-font "fontset-default" '(#x1100 . #xffdc)
                     '("NanumGothicCoding" . "iso10646-1"))
   (set-fontset-font "fontset-default" '(#xe0bc . #xf66e)
