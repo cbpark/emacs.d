abef021fe01280eb85ea76065cb9a7785c356295
cbpark <cbpark@gmail.com>
2016-11-29 18:05:37 +0900

    Add LaTeX-delete-environment function to init-auctex

--- lisp/init-auctex.el
+++ lisp/init-auctex.el
@@ -3,14 +3,13 @@
 ;;; Code:
 
 (require-package 'auctex)
-(setq auto-mode-alist
-      (append '(("\\.tex\\'" . latex-mode)) auto-mode-alist))
+(setq auto-mode-alist (append '(("\\.tex\\'" . latex-mode)) auto-mode-alist))
 
 (eval-after-load 'tex
   '(progn
-     (setq TeX-auto-save t)
-     (setq TeX-parse-self t)
-     (setq TeX-save-query nil)
+     (setq TeX-auto-save t
+           TeX-parse-self t
+           TeX-save-query nil)
      (setq-default TeX-master nil)
      (setq TeX-PDF-mode t)
      (setq TeX-view-program-selection
@@ -18,15 +17,15 @@
              (output-pdf "PDF Viewer")
              (output-html "HTML Viewer")))
 
-     (cond (*is-darwin* (setq TeX-view-program-list
-                              '(("DVI Viewer"  "/usr/bin/open -a TeXShop %o")
-                                ("PDF Viewer"  "/usr/bin/open -a Preview %o")
-                                ("HTML Viewer" "/usr/bin/open -a Safari %o"))))
-           ((string-equal system-type "gnu/linux")
-            (setq TeX-view-program-list
-                  '(("DVI Viewer"  "xdg-open %o")
-                    ("PDF Viewer"  "xdg-open %o")
-                    ("HTML Viewer" "xdg-open %o")))))))
+     (if *is-darwin*
+         (setq TeX-view-program-list
+               '(("DVI Viewer"  "/usr/bin/open -a TeXShop %o")
+                 ("PDF Viewer"  "/usr/bin/open -a Preview %o")
+                 ("HTML Viewer" "/usr/bin/open -a Safari %o")))
+       (setq TeX-view-program-list
+             '(("DVI Viewer"  "xdg-open %o")
+               ("PDF Viewer"  "xdg-open %o")
+               ("HTML Viewer" "xdg-open %o"))))))
 
 (require-package 'company-math)
 (defun company-math-setup ()
@@ -35,11 +34,34 @@
               (append '((company-math-symbols-latex company-latex-commands))
                       company-backends)))
 
+(defun LaTeX-delete-environment ()
+  "Delete the environment wrapper."
+  (interactive)
+  (when (LaTeX-current-environment)
+    (save-excursion
+      (let* ((begin-start (save-excursion
+                            (LaTeX-find-matching-begin)
+                            (point)))
+             (begin-end (save-excursion
+                          (goto-char begin-start)
+                          (search-forward-regexp "begin{.*?}")))
+             (end-end (save-excursion
+                        (LaTeX-find-matching-end)
+                        (point)))
+             (end-start (save-excursion
+                          (goto-char end-end)
+                          (1- (search-backward-regexp "\\end")))))
+        (delete-region end-start end-end)
+        (delete-region begin-start begin-end)))))
+
 (eval-after-load 'latex
   '(progn
      (setq latex-run-command "pdflatex")
      (setq LaTeX-default-style "article")
      (setq LaTeX-default-environment "align")
+
+     (define-key LaTeX-mode-map (kbd "C-c C-d") 'LaTeX-delete-environment)
+
      (add-hook 'LaTeX-mode-hook (lambda ()
                                   (company-math-setup)
                                   (flyspell-mode)
@@ -47,8 +69,8 @@
 
 (eval-after-load 'reftex
   '(progn
-     (setq reftex-plug-into-AUCTeX t)
-     (setq reftex-extra-bindings t)))
+     (setq reftex-plug-into-AUCTeX t
+           reftex-extra-bindings t)))
 
 ;; doc-view with auto-revert to review output
 (add-hook 'doc-view-mode-hook 'auto-revert-mode)
