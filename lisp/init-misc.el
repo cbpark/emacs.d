ca9c4c2621d8004eec7f296b977a5dfc1f2c3a79
cbpark <cbpark@gmail.com>
2016-12-03 00:37:34 +0900

    Update markdown mode

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -9,17 +9,15 @@
 ;; pkgbuild-mode for ArchLinux
 (when *is-linux*
   (require-package 'pkgbuild-mode)
-  (autoload 'pkgbuild-mode "pkgbuild-mode.el" "PKGBUILD mode." t)
+  (autoload 'pkgbuild-mode "pkgbuild-mode.el" "ArchLinux PKGBUILD mode." t)
   (setq auto-mode-alist (append '(("/PKGBUILD$" . pkgbuild-mode)) auto-mode-alist)))
 
 ;; unison mode
-(when (executable-find "unison")
-  (require-package 'unison-mode)
-  (autoload 'unison-mode "unison-mode" "my unison mode" t)
-  (setq auto-mode-alist (append '(("\\.prf$" . unison-mode)) auto-mode-alist))
-  (eval-after-load 'unison-mode
-    '(progn
-       (defvar unison-command "unison -ui text"))))
+(require-package 'unison-mode)
+(autoload 'unison-mode "unison-mode" "my unison mode" t)
+(setq auto-mode-alist (append '(("\\.prf$" . unison-mode)) auto-mode-alist))
+(eval-after-load 'unison-mode
+  '(defvar unison-command "unison -ui text"))
 
 ;; yaml mode
 (require-package 'yaml-mode)
@@ -30,13 +28,13 @@
 ;; markown mode
 (require-package 'markdown-mode)
 (setq auto-mode-alist
-      (append '(("\\.markdown$" . markdown-mode)
+      (append '(("README\\.md$" . gfm-mode)
+                ("\\.markdown$" . markdown-mode)
                 ("\\.md$"       . markdown-mode)) auto-mode-alist))
 (autoload 'markdown-mode "markdown-mode" "Markdown mode" t)
+(autoload 'gfm-mode "markdown-mode" "GitHub Flavored Markdown mode" t)
 (eval-after-load 'markdown-mode
-  '(progn
-     (setq markdown-command "multimarkdown")
-     (add-hook 'markdown-mode-hook 'turn-off-auto-fill)))
+  '(add-hook 'markdown-mode-hook 'turn-off-auto-fill))
 
 (provide 'init-misc)
 ;;; init-misc.el ends here
