8ac2dce917e600b31b4bed203e15f5f91fa83a77
cbpark <cbpark@gmail.com>
2016-12-07 01:13:45 +0900

    Make scratch in org-mode, update dict

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -11,5 +11,14 @@
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c b") 'org-iswitchb)
 
+;; Set the major mode of the initial *scratch* buffer to be org-mode
+(setq initial-major-mode 'org-mode)
+
+;; Turn TOC off
+(setq org-export-with-toc nil)
+
+(eval-after-load 'org
+  '(require 'ox-md nil t))
+
 (provide 'init-org)
 ;;; init-org.el ends here
