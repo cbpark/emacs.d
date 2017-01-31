4acb1969948e8d174595cd91be6432a29f27c79c
cbpark <cbpark@gmail.com>
2017-02-01 04:52:36 +0900

    Set python-shell-interpreter to be ipython

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -4,12 +4,16 @@
 
 (require 'python)
 (setq python-indent-offset 4)
+(when (executable-find "ipython")
+  (setq python-shell-interpreter "ipython"
+        python-shell-interpreter-args "-i")
+  (setenv "IPY_TEST_SIMPLE_PROMPT" "1"))
 
 ;; company-jedi
 (require-package 'company-jedi)
 (with-eval-after-load 'company
-  (add-hook 'python-mode-hook (lambda ()
-                                (add-to-list 'company-backends 'company-jedi))))
+  (add-hook 'python-mode-hook
+            (lambda () (add-to-list 'company-backends 'company-jedi))))
 
 (provide 'init-python)
 ;;; init-python.el ends here
