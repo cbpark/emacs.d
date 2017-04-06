535d88e9189182ffccfb409b32450cde49af4b34
cbpark <cbpark@gmail.com>
2017-04-06 21:14:12 +0900

    Remove settings for jk fonts, make the window transparent in darwin

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -2,17 +2,15 @@
 ;;; Commentary:
 ;;; Code:
 
-;; (require-package 'base16-theme)
-;; (load-theme 'base16-default-dark t)
 (require-package 'zenburn-theme)
 (defvar zenburn-override-colors-alist '(("zenburn-bg" . "#1f1f1f")))
 (load-theme 'zenburn t)
 (set-face-attribute 'region nil :background "#666" :foreground "#ffffff")
 
 ;; Transparent background.
-;; (when (and *is-linux* window-system)
-;;   (set-frame-parameter (selected-frame) 'alpha '(93 93))
-;;   (add-to-list 'default-frame-alist '(alpha 93 93)))
+(when (and *is-darwin* window-system)
+  (set-frame-parameter (selected-frame) 'alpha '(96 96))
+  (add-to-list 'default-frame-alist '(alpha 96 96)))
 
 ;; (defun on-frame-open (&optional frame)
 ;;   "If the FRAME created in the terminal don't load background color."
@@ -30,26 +28,6 @@
     (progn
       (setq initial-frame-alist '((font . "Source Code Pro-9")))
       (setq default-frame-alist '((font . "Source Code Pro-9"))))))
-
-(set-fontset-font "fontset-default" '(#x1100 . #xffdc)
-                  '("NanumGothicCoding" . "iso10646-1"))
-(set-fontset-font "fontset-default" '(#xe0bc . #xf66e)
-                  '("NanumGothicCoding" . "iso10646-1"))
-(set-fontset-font "fontset-default" 'kana
-                  '("Hiragino Kaku Gothic Pro" . "iso10646-1"))
-(set-fontset-font "fontset-default" 'han
-                  '("Hiragino Kaku Gothic Pro" . "iso10646-1"))
-(set-fontset-font "fontset-default" 'japanese-jisx0208
-                  '("Hiragino Kaku Gothic Pro" . "iso10646-1"))
-(set-fontset-font "fontset-default" 'japanese-jisx0213.2004-1
-                  '("Hiragino Kaku Gothic Pro" . "iso10646-1"))
-(set-fontset-font "fontset-default" 'japanese-jisx0213-2
-                  '("Hiragino Kaku Gothic Pro" . "iso10646-1"))
-;; half-width katakana
-(set-fontset-font "fontset-default" 'katakana-jisx0201
-                  '("Hiragino Kaku Gothic Pro" . "iso10646-1"))
-(setq face-font-rescale-alist '((".*hiragino.*" . 1.1)
-                                (".*nanum.*" . 1.1)))
 
 (provide 'init-themes)
 ;;; init-themes.el ends here
