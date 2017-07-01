648fe7295800932441065fd6dcd5be296e1f244d
cbpark <cbpark@gmail.com>
2017-07-01 19:40:30 +0900

    Remove company-nixos-options

--- lisp/init-nix.el
+++ lisp/init-nix.el
@@ -7,9 +7,5 @@
 (setq auto-mode-alist (append '(("\\.nix\\'"    . nix-mode)
                                 ("\\.nix.in\\'" . nix-mode)) auto-mode-alist))
 
-(require-package 'company-nixos-options)
-(with-eval-after-load 'company
-  (add-to-list 'company-backends 'company-nixos-options))
-
 (provide 'init-nix)
 ;;; init-nix.el ends here
