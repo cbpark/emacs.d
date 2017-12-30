9477c7d457ed47ed89336238ae543d705629d9fc
cbpark <cbpark@gmail.com>
2017-12-31 02:31:53 +0900

    Remove pkgbuild-mode, cmake-mode, and yaml-mode

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -2,19 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-;; pkgbuild-mode for ArchLinux
-(when *is-linux*
-  (require-package 'pkgbuild-mode)
-  (autoload 'pkgbuild-mode "pkgbuild-mode.el" "ArchLinux PKGBUILD mode." t)
-  (setq auto-mode-alist
-        (append '(("/PKGBUILD$" . pkgbuild-mode)) auto-mode-alist)))
-
-;; yaml mode
-(require-package 'yaml-mode)
-(autoload 'yaml-mode "yaml-mode" "YAML mode" t)
-(setq auto-mode-alist (append '(("\\.yaml$" . yaml-mode)
-                                ("\\.yml$"  . yaml-mode)) auto-mode-alist))
-
 ;; markown mode
 (require-package 'markdown-mode)
 (setq auto-mode-alist
@@ -25,16 +12,6 @@
 (autoload 'gfm-mode "markdown-mode" "GitHub Flavored Markdown mode" t)
 (with-eval-after-load 'markdown-mode
   (add-hook 'markdown-mode-hook 'turn-off-auto-fill))
-
-;; cmake mode
-(if (file-exists-p (concat *site-lisp-dir* "cmake-mode.el"))
-    (require 'cmake-mode)
-  (require-package 'cmake-mode))
-
-;; nix mode
-(when (file-directory-p "~/.nix-profile/share/emacs")
-  (add-to-list 'load-path "~/.nix-profile/share/emacs/site-lisp")
-  (require 'nix-mode))
 
 ;; syntax highlighting for sysmtemd files
 (add-to-list 'auto-mode-alist '("\\.service\\'" . conf-unix-mode))
