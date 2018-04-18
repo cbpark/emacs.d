46d04bd2439a93c6747732959f3eddaddb081972
cbpark <cbpark@gmail.com>
2018-04-18 20:34:09 +0900

    Use shell-script-mode in PKGBUILD files

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -14,18 +14,21 @@
   (add-hook 'markdown-mode-hook 'turn-off-auto-fill))
 
 ;; syntax highlighting for sysmtemd files
-(add-to-list 'auto-mode-alist '("\\.service\\'" . conf-unix-mode))
-(add-to-list 'auto-mode-alist '("\\.timer\\'" . conf-unix-mode))
-(add-to-list 'auto-mode-alist '("\\.target\\'" . conf-unix-mode))
-(add-to-list 'auto-mode-alist '("\\.mount\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.service\\'"   . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.timer\\'"     . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.target\\'"    . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.mount\\'"     . conf-unix-mode))
 (add-to-list 'auto-mode-alist '("\\.automount\\'" . conf-unix-mode))
-(add-to-list 'auto-mode-alist '("\\.slice\\'" . conf-unix-mode))
-(add-to-list 'auto-mode-alist '("\\.socket\\'" . conf-unix-mode))
-(add-to-list 'auto-mode-alist '("\\.path\\'" . conf-unix-mode))
-(add-to-list 'auto-mode-alist '("\\.netdev\\'" . conf-unix-mode))
-(add-to-list 'auto-mode-alist '("\\.network\\'" . conf-unix-mode))
-(add-to-list 'auto-mode-alist '("\\.link\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.slice\\'"     . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.socket\\'"    . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.path\\'"      . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.netdev\\'"    . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.network\\'"   . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.link\\'"      . conf-unix-mode))
 (add-to-list 'auto-mode-alist '("\\.automount\\'" . conf-unix-mode))
+
+;; PKGBUILD files
+(add-to-list 'auto-mode-alist '("/PKGBUILD$" . shell-script-mode))
 
 (provide 'init-misc)
 ;;; init-misc.el ends here
