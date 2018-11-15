ecf04b9122250a11f1bb71a0c422c1d9d862e3fe
cbpark <cbpark@gmail.com>
2018-11-15 14:50:38 +0900

    Remove company-jedi

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -9,11 +9,5 @@
         python-shell-interpreter-args "-i")
   (setenv "IPY_TEST_SIMPLE_PROMPT" "1"))
 
-;; company-jedi
-(require-package 'company-jedi)
-(with-eval-after-load 'company
-  (add-hook 'python-mode-hook (lambda ()
-                                (add-to-list 'company-backends 'company-jedi))))
-
 (provide 'init-python)
 ;;; init-python.el ends here
