f9ac4c2921d4b517bcb5d41eb50a74b712d23df2
cbpark <cbpark@gmail.com>
2018-12-29 23:41:18 +0900

    Modify python-shell-interpreter

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -6,19 +6,18 @@
 (setq python-indent-offset 4)
 (when (executable-find "ipython")
   (setq python-shell-interpreter "ipython"
-        python-shell-interpreter-args "-i")
-  (setenv "IPY_TEST_SIMPLE_PROMPT" "1"))
-
-(require-package 'elpy)
-(elpy-enable)
-(with-eval-after-load 'elpy
-  (defalias 'workon 'pyvenv-workon))
+        python-shell-interpreter-args "-i --simple-prompt"))
 
 (add-hook 'inferior-python-mode-hook
           (lambda ()
             (linum-mode -1)
             (setq-local global-hl-line-mode nil)
             (undo-tree-mode -1)))
+
+(require-package 'elpy)
+(elpy-enable)
+(with-eval-after-load 'elpy
+  (defalias 'workon 'pyvenv-workon))
 
 (provide 'init-python)
 ;;; init-python.el ends here
