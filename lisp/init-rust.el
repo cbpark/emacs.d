509d7812bad4d90eebdce307c97cf40000780214
cbpark <cbpark@gmail.com>
2019-07-10 21:41:41 +0900

    Add racer to rust-mode

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -9,7 +9,7 @@
 ;; flycheck
 (require-package 'flycheck-rust)
 (with-eval-after-load 'rust-mode
-  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+  (add-hook 'flycheck-mode-hook 'flycheck-rust-setup))
 
 ;; rustfmt
 (when (executable-find "rustfmt")
@@ -17,7 +17,15 @@
     (setq rust-format-on-save nil)
     (add-hook 'rust-mode-hook
               (lambda ()
-                (local-set-key (kbd "C-c l") #'rust-format-buffer)))))
+                (local-set-key (kbd "C-c l") 'rust-format-buffer)))))
+
+;; racer
+(require-package 'racer)
+(when (executable-find "racer")
+  (with-eval-after-load 'rust-mode
+    (add-hook 'rust-mode-hook 'racer-mode)
+    (dolist (minor-modes '(eldoc-mode company-mode))
+      (add-hook 'racer-mode-hook minor-modes))))
 
 (provide 'init-rust)
 ;;; init-rust.el ends here
