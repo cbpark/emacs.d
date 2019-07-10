38f6e12209e374239591c5f8f7dc55bf5a9c6681
cbpark <cbpark@gmail.com>
2019-07-10 23:48:14 +0900

    Update init-rust (flyspell)

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -6,11 +6,6 @@
 (require-package 'cargo)
 (add-hook 'rust-mode-hook 'cargo-minor-mode)
 
-;; flycheck
-(require-package 'flycheck-rust)
-(with-eval-after-load 'rust-mode
-  (add-hook 'flycheck-mode-hook 'flycheck-rust-setup))
-
 ;; rustfmt
 (when (executable-find "rustfmt")
   (with-eval-after-load 'rust-mode
@@ -20,12 +15,22 @@
                 (local-set-key (kbd "C-c l") 'rust-format-buffer)))))
 
 ;; racer
-(require-package 'racer)
 (when (executable-find "racer")
+  (require-package 'racer)
   (with-eval-after-load 'rust-mode
     (add-hook 'rust-mode-hook 'racer-mode)
     (dolist (minor-modes '(eldoc-mode company-mode))
       (add-hook 'racer-mode-hook minor-modes))))
+
+;; flycheck
+(require-package 'flycheck-rust)
+(with-eval-after-load 'rust-mode
+  (add-hook 'flycheck-mode-hook 'flycheck-rust-setup))
+
+;; flyspell
+(when *has-aspell*
+  (with-eval-after-load 'rust-mode
+    (add-hook 'rust-mode-hook 'flyspell-prog-mode)))
 
 (provide 'init-rust)
 ;;; init-rust.el ends here
