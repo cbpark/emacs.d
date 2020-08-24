1d4174cdead392abbd7d2dcbfa74a444c3edccbd
cbpark <cbpark@gmail.com>
2020-08-24 11:39:44 +0900

    Require cl for imaxima

--- lisp/init-maxima.el
+++ lisp/init-maxima.el
@@ -13,25 +13,26 @@
         (append '(("\\.ma[cx]\\'" . maxima-mode)
                   ("\\.mc\\'"     . maxima-mode)) auto-mode-alist))
 
-  (defun open-imaxima ()
-    "Open `imaxima' after splitting the window."
-    (interactive)
-    (save-buffer)
-    (split-window-sensibly)
-    (imaxima)
-    (my-swap-two-windows))
+  ;; (defun open-imaxima ()
+  ;;   "Open `imaxima' after splitting the window."
+  ;;   (interactive)
+  ;;   (save-buffer)
+  ;;   (split-window-sensibly)
+  ;;   (imaxima)
+  ;;   (my-swap-two-windows))
 
   (with-eval-after-load 'maxima
     (define-key maxima-mode-map (kbd "C-c C-r") 'maxima-send-region)
     (define-key maxima-mode-map (kbd "C-c C-b") 'maxima-send-buffer)
     (define-key maxima-mode-map (kbd "C-c C-c") 'maxima-send-line)
     (define-key maxima-mode-map (kbd "C-c C-l") 'maxima-load-file)
-    (define-key maxima-mode-map (kbd "C-c C-i") 'open-imaxima)
+    ;; (define-key maxima-mode-map (kbd "C-c C-i") 'open-imaxima)
     (define-key maxima-mode-map (kbd "C-c C-k") 'maxima-stop))
 
   (add-hook 'inferior-maxima-mode-hook (lambda () (undo-tree-mode -1)))
 
   ;; Imaxima
+  (require 'cl)  ;; deprecated
   (autoload 'imaxima "imaxima" "Image support for Maxima." t)
   (with-eval-after-load 'imaxima
     (setq imaxima-maxima-program "maxima")
