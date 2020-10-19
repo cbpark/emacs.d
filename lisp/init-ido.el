815b47fca20ddab7bc6923bf6df289e4fc0f40ef
cbpark <cbpark@gmail.com>
2020-10-19 22:29:11 +0900

    Remove some default customizations for ido mode

--- lisp/init-ido.el
+++ lisp/init-ido.el
@@ -5,16 +5,11 @@
 (require 'ido)
 (ido-mode 1)
 
-(setq ido-case-fold t
-      ido-confirm-unique-completion t
+(setq ido-confirm-unique-completion t
       ido-create-new-buffer 'always
       ido-enable-flex-matching t
       ido-enable-last-directory-history t
       ido-everywhere t
-      ido-ignore-extensions t
-      ido-max-prospects 10
-      ido-use-filename-at-point nil
-      ido-use-url-at-point nil
       ido-use-virtual-buffers t)
 
 (setq ido-ignore-buffers '("\\` " "^\\*.*buffer\\*$" "^\\*GNU Emacs\\*$"
@@ -28,14 +23,13 @@
 ;; icomplete
 (icomplete-mode t)
 
-(global-set-key (kbd "C-x C-m")
-                (lambda ()
-                  (interactive)
-                  (call-interactively
-                   (intern
-                    (ido-completing-read
-                     "M-x "
-                     (all-completions "" obarray 'commandp))))))
+(global-set-key (kbd "C-x C-m") (lambda ()
+                                  (interactive)
+                                  (call-interactively
+                                   (intern
+                                    (ido-completing-read
+                                     "M-x "
+                                     (all-completions "" obarray 'commandp))))))
 
 (provide 'init-ido)
 ;;; init-ido.el ends here
