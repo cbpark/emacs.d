4f8441e59f739a2a2b5a4fdda794934823d10da2
cbpark <cbpark@gmail.com>
2021-01-03 22:21:01 +0900

    Simplify the font setup, turn on transparency, use the on-frame-open function

--- lisp/init-face.el
+++ lisp/init-face.el
@@ -13,7 +13,6 @@
 ;; (setq base16-theme-256-color-source "colors"
 ;;       base16-highlight-mode-line 'contrast
 ;;       base16-distinct-fringe-background nil)
-;; ;; (load-theme 'base16-tomorrow-night t)
 ;; (load-theme 'base16-default-dark t)
 ;; (setq default-frame-alist '((cursor-color . "#ab4642")))
 
@@ -24,33 +23,17 @@
 (doom-modeline-mode 1)
 
 ;; Transparent background.
-(when window-system
-  (set-frame-parameter (selected-frame) 'alpha '(96 96))
-  (add-to-list 'default-frame-alist '(alpha 96 96)))
+(set-frame-parameter (selected-frame) 'alpha '(96 96))
+(add-to-list 'default-frame-alist '(alpha 96 96))
 
-;; (defun on-frame-open (&optional frame)
-;;   "If the FRAME created in the terminal don't load background color."
-;;   (unless (display-graphic-p frame)
-;;     (set-face-background 'default "unspecified-bg" frame)))
-;; (add-hook 'after-make-frame-functions 'on-frame-open)
-;; (add-hook 'window-setup-hook 'on-frame-open)
+(defun on-frame-open (&optional frame)
+  "If the FRAME created in the terminal don't load background color."
+  (unless (display-graphic-p frame)
+    (set-face-background 'default "unspecified-bg" frame)))
+(add-hook 'after-make-frame-functions 'on-frame-open)
 
 ;; Font
-(defconst *default-font* "Source Code Pro")
-
-(when (member *default-font* (font-family-list))
-  (cond (*is-darwin*
-         (set-face-attribute 'default nil :font *default-font* :height 120))
-        (t
-         (set-face-attribute 'default nil :font *default-font* :height 90))))
-
-(defun my-set-default-font (frame)
-  "Set default font on FRAME creation."
-  (with-selected-frame frame
-    (when (and (member *default-font* (font-family-list)) (display-graphic-p))
-      (set-frame-font (concat *default-font* "-9")))))
-
-(when *is-linux* (add-hook 'after-make-frame-functions 'my-set-default-font))
+(add-to-list 'default-frame-alist '(font . "Source Code Pro-9"))
 
 (provide 'init-face)
 ;;; init-face.el ends here
