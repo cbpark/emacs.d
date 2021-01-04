3d14850d4f4c7677f356e8d8ad4860a07cfbe6f7
cbpark <cbpark@gmail.com>
2021-01-04 20:04:55 +0900

    Update init-ido (vertical display etc)

--- lisp/init-ido.el
+++ lisp/init-ido.el
@@ -10,7 +10,11 @@
       ido-enable-flex-matching t
       ido-enable-last-directory-history t
       ido-everywhere t
-      ido-use-virtual-buffers t)
+      ido-use-virtual-buffers t
+      ido-case-fold t
+      ido-show-dot-for-dired t
+      ido-use-filename-at-point nil
+      ido-default-file-method 'selected-window)
 
 (setq ido-ignore-buffers '("\\` " "^\\*.*buffer\\*$" "^\\*GNU Emacs\\*$"
                            "\\.log$" "^.*Help\\*$" "^\\*Warnings\\*$"
@@ -18,8 +22,21 @@
                            "\\.projectile$" "^.*compilation\\*$"
                            "^\\*Completions\\*$" "^\\*magit-process\\*$"))
 (setq ido-ignore-files (quote (".DS_Store" ".localized" "Thumbs.db"
-                               "desktop.ini" "*.aux")))
+                               "desktop.ini" "*.aux" "ido.last")))
 
+;; Display completions vertically
+(setq ido-decorations
+      (quote ("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
+(setq max-mini-window-height 0.2)
+(add-hook 'ido-minibuffer-setup-hook
+          (lambda ()
+            (set (make-local-variable 'truncate-lines) nil)))
+(defun ido-define-keys ()
+  (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
+  (define-key ido-completion-map (kbd "C-p") 'ido-prev-match))
+(add-hook 'ido-setup-hook 'ido-define-keys)
+
+;; M-x mode
 (global-set-key (kbd "C-x C-m") (lambda ()
                                   (interactive)
                                   (call-interactively
