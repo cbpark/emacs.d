36d0df574187c3e79bf371c9b9086497cdcd0abf
cbpark <cbpark@gmail.com>
2021-01-10 16:46:05 +0900

    Modify init-maxima

--- lisp/init-maxima.el
+++ lisp/init-maxima.el
@@ -13,34 +13,23 @@
         (append '(("\\.ma[cx]\\'" . maxima-mode)
                   ("\\.mc\\'"     . maxima-mode)) auto-mode-alist))
 
-  ;; (defun open-imaxima ()
-  ;;   "Open `imaxima' after splitting the window."
-  ;;   (interactive)
-  ;;   (save-buffer)
-  ;;   (split-window-sensibly)
-  ;;   (imaxima)
-  ;;   (my-swap-two-windows))
-
   (with-eval-after-load 'maxima
     (define-key maxima-mode-map (kbd "C-c C-r") 'maxima-send-region)
     (define-key maxima-mode-map (kbd "C-c C-b") 'maxima-send-buffer)
     (define-key maxima-mode-map (kbd "C-c C-c") 'maxima-send-line)
     (define-key maxima-mode-map (kbd "C-c C-l") 'maxima-load-file)
-    ;; (define-key maxima-mode-map (kbd "C-c C-i") 'open-imaxima)
     (define-key maxima-mode-map (kbd "C-c C-k") 'maxima-stop))
 
   (add-hook 'inferior-maxima-mode-hook (lambda () (undo-tree-mode -1)))
 
   ;; Imaxima
-  (require 'cl)  ;; deprecated
-  (autoload 'imaxima "imaxima" "Image support for Maxima." t)
+  (require 'cl)
+  (autoload 'imaxima "imaxima" "Frontend for maxima with Image support" t)
+  (autoload 'imath-mode "imath" "Imath mode for math formula input" t)
   (with-eval-after-load 'imaxima
     (setq imaxima-maxima-program "maxima")
     (setq imaxima-use-maxima-mode-flag t)
-    (setq imaxima-pt-size 12)
-    (if *is-darwin*
-        (setq imaxima-fnt-size "large")
-      (setq imaxima-fnt-size "LARGE"))))
+    (setq imaxima-pt-size 12)))
 
 (provide 'init-maxima)
 ;;; init-maxima.el ends here
